cb74ab7d34c474aae43a2259dbf7297d7cec00d1
John Ohno <john.ohno@gmail.com>
2013-11-08 16:51:17 -0500

    clean up

--- fav.sh
+++ fav.sh
@@ -1,3 +1,15 @@
-prolog -q -s <(cat nell.pl questions.pl) -t "questions(X, Y),writeln(X), writeln(Y), false." 2>/dev/null | sed 's/^.\([a-z][^|]*\)|\(.*\).$/\1, because it is also \2\n/' | tr -d '[]|' | tee favorite_generated.txt
-cat favorite_generated.txt| sed 's/^W\(.*\)$/W\1?/' | sed 's/^$/%/' | tr -d '\n' | tr '%' '\n' | sort | uniq | sed 's/$/\n/;s/?/?\n/' | sed 's/^\([^W].*\)$/\1./' | python <(echo -e "import sys\nfor x in sys.stdin.readlines():\n\tprint(x.capitalize().rstrip())") | sed 's/kind of //' | tee favorite_generated_clean.txt
+#!/usr/bin/env zsh
+prolog -q -s <(cat nell.pl questions.pl) -t "questions(X, Y),writeln(X), writeln(Y), false." 2>/dev/null | 
+	sed 's/^.\([a-z][^|]*\)|\(.*\).$/\1, because it is also \2\n/' |	# format our response appropriately 
+	tr -d '[]|' | 								# get rid of prolog list format
+	tee favorite_generated.txt | 						# save to a temp file
+	sed 's/^W\(.*\)$/W\1?/' | 						# add question marks to the ends of the questions
+	sed 's/^$/%/' | 							# separate stanzas
+	tr -d '\n' | tr '%' '\n' | 
+	sort | uniq | 								# deduplicate
+	sed 's/$/\n/;s/?/?\n/' | 
+	sed 's/^\([^W].*\)$/\1./' | 						# add periods to the ends of the responses
+	python <(echo -e "import sys\nfor x in sys.stdin.readlines():\n\tprint(x.capitalize().rstrip())") | # capitalize
+	sed 's/kind of //' | 							# your favorite KIND OF actor isn't tom cruise
+	tee favorite_generated_clean.txt
 
--- nell2pl.sh
+++ nell2pl.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
-tr -d "'" | sed 's/\t\t*/\t/g;s/\t$//;s/^\([^\t]*\)\t\([^\t]*\)\t\([^\t]*\).*$/?- assert(\2('"'"'\1'"'"', '"'"'\3'"'"'))./;s/,/'"'"', '"'"'/g;s/'"'"'[ ]*'"'"'/'"'"'/g' | tr 'A-Z' 'a-z' | 
+tr -d "'" | sed 's/\t\t*/\t/g;s/\t$//;s/^\([^\t]*\)\t\([^\t]*\)\t\([^\t]*\).*$/?- assert(\2('"'"'\1'"'"', '"'"'\3'"'"'))./;s/,/'"'"', '"'"'/g;s/'"'"'[ ]*'"'"'/'"'"'/g' | tr 'A-Z' 'a-z' | # jesus christ. this looked cleaner when I wrote it, I promise. 
 	sed "s/\([(']\)[^(,']*:/\1/g;s/_/ /g" | 
-	sort | uniq
+	sort | uniq	# comment out this line to save processing time & memory at the cost of having shitloads of duplicate preds
