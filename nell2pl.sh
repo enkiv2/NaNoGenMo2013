4e7ff5912a4a9b622eda28e2ddeeb441e04ac95f
John Ohno <john.ohno@gmail.com>
2013-11-08 16:56:58 -0500

    clean up

--- nell2pl.sh
+++ nell2pl.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env zsh
-tr -d "'" | sed 's/\t\t*/\t/g;s/\t$//;s/^\([^\t]*\)\t\([^\t]*\)\t\([^\t]*\).*$/?- assert(\2('"'"'\1'"'"', '"'"'\3'"'"'))./;s/,/'"'"', '"'"'/g;s/'"'"'[ ]*'"'"'/'"'"'/g' | tr 'A-Z' 'a-z' | # jesus christ. this looked cleaner when I wrote it, I promise. 
-	sed "s/\([(']\)[^(,']*:/\1/g;s/_/ /g" | 
-	sort | uniq	# comment out this line to save processing time & memory at the cost of having shitloads of duplicate preds
+tr -d "'" | 
+	sed 's/\t\t*/\t/g;s/\t$//;
+	s/^\([^\t]*\)\t\([^\t]*\)\t\([^\t]*\).*$/?- assert(\2('"'"'\1'"'"', '"'"'\3'"'"'))./;
+	s/,/'"'"', '"'"'/g;s/'"'"'[ ]*'"'"'/'"'"'/g' |	# sed: strip trailing/repeated tabs
+							# turn Event\tRelation\tDetail into fact of form Relation('Event', 'Detail').
+							# then, strip out common quoting errors
+	tr 'A-Z' 'a-z' | # capital letters are meaningful in prolog; use all-lowercase to make literals atoms
+	sed "s/\([(']\)[^(,']*:/\1/g;
+	s/_/ /g" | 	# handle another common quoting mistake
+			# then substitute underscores for spaces, since NELL seems to have substituted whitespace for underscores already
+	sort | uniq |	# comment out this line to save processing time & memory at the cost of having shitloads of duplicate preds
+	cat
+
