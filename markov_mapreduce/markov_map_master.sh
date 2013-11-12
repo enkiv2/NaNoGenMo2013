e9885166486272b52a15287832f5352940c956bc
ohno <john.ohno@gmail.com>
2013-11-12 18:46:58 -0500

    implement proper word shifting

--- markov_mapreduce/markov_map_master.sh
+++ markov_mapreduce/markov_map_master.sh
@@ -6,7 +6,11 @@ threads=$2
 shift; shift;
 
 for i in $@ ; do
-	cat $i |sed 's/$/ \\n /;s/\t/ \\t /g' | tr ' \t\r' '\n\n\n' | tr -d '"'"'"'' | grep . | xargs -n$order -P$threads ./markov_map_slave.sh
+	j=1
+	while [[ $j -lt $order ]] ; do
+		cat $i| sed 's/$/ \\n /;s/\t/ \\t /g' | tr ' \t\r' '\n\n\n' | tr -d '"'"'"'' | grep . | tail -n +$j | xargs -n$order -P$threads ./markov_map_slave.sh
+		j=$((j+1))
+	done
 	echo "Mapped $i"
 done
 
