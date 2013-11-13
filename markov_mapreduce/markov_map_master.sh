46a10dae99161bbfff9dd622b8da654bf5973b9b
ohno <john.ohno@gmail.com>
2013-11-13 07:37:10 -0500

    add more indicators, and perform reductions more often

--- markov_mapreduce/markov_map_master.sh
+++ markov_mapreduce/markov_map_master.sh
@@ -8,7 +8,15 @@ shift; shift;
 for i in $@ ; do
 	j=1
 	while [[ $j -lt $order ]] ; do
-		cat $i| sed 's/$/ \\n /;s/\t/ \\t /g' | tr ' \t\r' '\n\n\n' | tr -d '"'"'"'' | grep . | tail -n +$j | xargs -n$order -P$threads ./markov_map_slave.sh
+		cat $i| tr ' \t\r' '\n\n\n' | tr -d '"'"'"'' | grep . | tail -n +$j | xargs -n$order -P$threads ./markov_map_slave.sh
+		echo "$(((j*100)/order))%"
+		echo -e "Reducing... \c"
+		./markov_reduce_slave.sh markov.* > .$$
+		echo "Reduced!"
+		echo -e "Cleaning up... \c"
+		rm markov.*
+		mv .$$ markov.out
+		echo "Clean!"
 		j=$((j+1))
 	done
 	echo "Mapped $i"
